9d6645f5d2be3e6f1dee86c0dd512d3d31b94262
Ferdibrand <david.detombe@gmail.com>
2026-06-10 15:33:16 +0200

    add basic add_time function and tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use anyhow::Result;
+
+#[derive(Debug, PartialEq)]
+pub struct Solve {
+    time: u32,
+}
+
+impl Solve {
+    pub fn time(&self) -> Option<u32> {
+        Some(self.time)
+    }
+}
+
+pub fn add_time(solves: &mut Vec<Solve>, input: &str) -> Result<()> {
+    let time: u32 = input.parse()?;
+    let solve = Solve { time };
+    solves.push(solve);
+    Ok(())
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_time_adds_time() {
+        let mut solves: Vec<Solve> = Vec::new();
+        let _ = add_time(&mut solves, "31415");
+        let solve = Solve { time: 31415 };
+        assert_eq!(solves[0], solve, "did not add time");
+    }
+
+    #[test]
+    fn test_add_time_rejects_invalid_time() {
+        let mut solves: Vec<Solve> = Vec::new();
+        let result = add_time(&mut solves, "abc");
+        assert!(result.is_err())
+    }
+
+    #[test]
+    fn test_time_returns_time() {
+        let solve = Solve { time: 31415 };
+        let time = solve.time().unwrap();
+        assert_eq!(time, 31415, "incorrect time");
+    }
 }
