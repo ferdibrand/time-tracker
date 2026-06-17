6d93e2b5527f197305666a2b5de316e2945f0947
Ferdibrand <david.detombe@gmail.com>
2026-06-17 20:51:50 +0200

    add average stuff

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,183 @@
+#![allow(dead_code, unused)]
+use thiserror::Error;
+
+use crate::AverageKind::TrimmedMean;
+
 #[derive(Debug, PartialEq, Clone)]
-pub struct Solve {
-    pub time: u32,
+struct Solve {
+    time: u32,
+    penalty: Penalty,
+}
+
+impl Solve {
+    fn new(time: u32, penalty: Penalty) -> Self {
+        Solve { time, penalty }
+    }
+
+    fn effective_time(&self) -> Option<u32> {
+        match self.penalty {
+            Penalty::None => Some(self.time),
+            Penalty::Plus2 => Some(self.time + 2000),
+            Penalty::Dnf => None,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
-pub enum Average {
+enum Average {
     Time(u32),
     Dnf,
     Incomplete,
 }
 
+enum AverageKind {
+    Mean,
+    TrimmedMean(usize),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Penalty {
+    None,
+    Plus2,
+    Dnf,
+}
+
 #[derive(Debug, PartialEq, Default)]
-pub struct Session {
-    pub solves: Vec<Solve>,
+struct Session {
+    solves: Vec<Solve>,
 }
 
 impl Session {
-    pub fn add_time(&mut self, time: u32) {
-        let solve = Solve { time };
+    fn add_time(&mut self, time: u32, penalty: Penalty) {
+        let solve = Solve { time, penalty };
         self.solves.push(solve);
     }
 
-    pub fn add_times(&mut self, times: Vec<u32>) {
+    fn add_times(&mut self, times: Vec<u32>) {
         for time in times {
-            self.add_time(time);
+            self.add_time(time, Penalty::None);
         }
     }
 
-    pub fn best_solve(&self) -> Option<Solve> {
-        self.solves.iter().min_by_key(|solve| solve.time).cloned()
-    }
-
-    pub fn latest_ao(&self, num_solves: u32) -> Average {
-        let latest = self.solves[self.solves.len() - num_solves as usize..].to_vec();
-        Average::Time(Self::trimmed_mean(latest, 0.05))
-    }
-
-    fn trimmed_mean(mut solves: Vec<Solve>, trim_amount: f64) -> u32 {
-        solves.sort_by_key(|solve| solve.time);
-        let trim_number = (solves.len() as f64 * trim_amount).ceil();
-        let trimmed = solves[trim_number as usize..solves.len() - trim_number as usize].to_vec();
-        let mut sum = 0;
-        for solve in &trimmed {
-            sum += solve.time;
-        }
-        sum / trimmed.len() as u32
+    fn best_solve(&self) -> Option<&Solve> {
+        self.solves.iter().min_by_key(|s| match s.effective_time() {
+            Some(time) => (0, time),
+            None => (1, 0),
+        })
     }
 }
+
+fn average(solves: &[Solve], kind: AverageKind) -> Result<Average, AverageError> {
+    let len = solves.len();
+    let trim = match kind {
+        AverageKind::Mean => 0,
+        AverageKind::TrimmedMean(amount) => (len * amount + 99) / 100,
+    };
+
+    if len < trim * 2 + 1 {
+        return Err(AverageError::NotEnoughSolves {
+            expected: trim * 2 + 1,
+            found: len,
+        });
+    }
+
+    let mut sorted = solves.to_vec();
+    sorted.sort_by_key(|s| match s.effective_time() {
+        Some(time) => (0, time),
+        None => (1, 0),
+    });
+    let trimmed = &sorted[trim..len - trim];
+    if trimmed.iter().any(|s| s.effective_time().is_none()) {
+        return Ok(Average::Dnf);
+    }
+
+    let sum: u32 = trimmed.iter().map(|s| s.effective_time().unwrap()).sum();
+    Ok(Average::Time(sum / trimmed.len() as u32))
+}
+
+#[derive(Debug, Error)]
+enum AverageError {
+    #[error("need at least {expected} solves, received {found}")]
+    NotEnoughSolves { expected: usize, found: usize },
+}
+
+fn main() {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::assert_matches;
 
     #[test]
-    fn add_time_fn_adds_time() {
+    fn add_time_adds_time() {
         let mut session = Session::default();
-        let _ = session.add_time(1);
-        let solve = Solve { time: 1 };
+        let _ = session.add_time(1, Penalty::None);
+        let solve = Solve {
+            time: 1,
+            penalty: Penalty::None,
+        };
         assert_eq!(session.solves[0], solve, "did not add time");
     }
 
     #[test]
-    fn add_times_fn_adds_times() {
+    fn add_times_adds_times() {
         let mut session = Session::default();
         session.add_times(vec![1, 2, 3, 4, 5]);
         assert_eq!(session.solves.len(), 5, "did not add times")
     }
 
     #[test]
-    fn best_solve_fn_returns_best_solve() {
+    fn best_solve_returns_best_solve() {
         let mut session = Session::default();
         session.add_times(vec![1, 2, 3]);
         let best_solve = session.best_solve().unwrap();
-        assert_eq!(best_solve, Solve { time: 1 }, "incorrect time")
+        assert_eq!(
+            *best_solve,
+            Solve {
+                time: 1,
+                penalty: Penalty::None
+            },
+            "incorrect time"
+        )
     }
 
     #[test]
-    fn latest_ao5_fn_returns_correct_time() {
-        let mut session = Session::default();
-        session.add_times(vec![1, 2, 3, 4, 5, 6, 50]);
-        let average: Average = session.latest_ao(5);
-        assert_eq!(average, Average::Time(5));
+    fn average_trimmed_mean_is_correct() {
+        let solves = vec![
+            Solve::new(12, Penalty::None),
+            Solve::new(13, Penalty::None),
+            Solve::new(14, Penalty::None),
+            Solve::new(16, Penalty::None),
+            Solve::new(999, Penalty::Dnf),
+        ];
+        let average: Average = average(&solves, AverageKind::TrimmedMean(5)).unwrap();
+        assert_eq!(average, Average::Time(14), "incorrect average");
+    }
+
+    #[test]
+    fn average_is_dnf_with_multiple_dnf_solves() {
+        let solves = vec![
+            Solve::new(12, Penalty::None),
+            Solve::new(13, Penalty::None),
+            Solve::new(14, Penalty::Dnf),
+            Solve::new(16, Penalty::None),
+            Solve::new(999, Penalty::Dnf),
+        ];
+        let average = average(&solves, AverageKind::TrimmedMean(5)).unwrap();
+        assert_matches!(average, Average::Dnf, "should have returned DNF");
+    }
+
+    #[test]
+    fn average_errors_when_too_few_solves() {
+        let solves = vec![Solve::new(12, Penalty::None), Solve::new(13, Penalty::None)];
+        let result = average(&solves, AverageKind::TrimmedMean(5));
+        assert_matches!(
+            result,
+            Err(AverageError::NotEnoughSolves {
+                expected: _,
+                found: _
+            }),
+            "should have returned NotEnoughSolves error",
+        );
     }
 }
